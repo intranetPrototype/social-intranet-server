1d1822ee9ce835583d80213c939a044103282b10
Sautino <marcelsauter9299@gmail.com>
2023-01-25 17:55:50 +0100

    Add swagger-documentation for auth controller

--- src/auth/model/request/update-user-password.request.ts
+++ src/auth/model/request/update-user-password.request.ts
@@ -1,6 +1,11 @@
+import { ApiProperty } from "@nestjs/swagger/dist/decorators";
 import { IsNotEmpty, IsString } from "class-validator";
 
 export class UpdateUserPasswordRequest {
+  @ApiProperty({
+    description: 'The new updated user password',
+    default: 'updated-password'
+  })
   @IsNotEmpty()
   @IsString()
   password: string;
--- src/auth/types/tokens.type.ts
+++ src/auth/types/tokens.type.ts
@@ -1,4 +1,15 @@
-export type Tokens = {
+import { ApiProperty } from "@nestjs/swagger";
+
+export class Tokens {
+  @ApiProperty({
+    description: 'User access-token',
+    default: 'access-token'
+  })
   access_token: string;
+
+  @ApiProperty({
+    description: 'User refresh-token to get new access-token if expired',
+    default: 'refresh-token'
+  })
   refresh_token: string;
 }
