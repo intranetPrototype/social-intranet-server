efae80c10368d9a3c29f7338f04622c261d420d1
Marcel Sauter <msauter@netconnections.de>
2023-02-06 14:48:15 +0100

    Add firstName and lastName to user db model

--- src/auth/model/dto/create-user.dto.ts
+++ src/auth/model/dto/create-user.dto.ts
@@ -3,6 +3,14 @@ import { IsEmail, IsNotEmpty, IsOptional, IsString } from "class-validator";
 
 export class CreateUserDto {
   @IsNotEmpty()
+  @IsString()
+  firstName: string;
+
+  @IsNotEmpty()
+  @IsString()
+  lastName: string;
+
+  @IsNotEmpty()
   @IsEmail()
   email: string;
 
--- src/auth/model/request/signup-user-request.ts
+++ src/auth/model/request/signup-user-request.ts
@@ -3,6 +3,22 @@ import { IsNotEmpty, IsEmail, IsString } from "class-validator";
 
 export class SignupUserRequest {
   @ApiProperty({
+    description: 'First name of the registered user',
+    default: 'John'
+  })
+  @IsNotEmpty()
+  @IsString()
+  firstName: string;
+
+  @ApiProperty({
+    description: 'Last name of the registered user',
+    default: 'Doe'
+  })
+  @IsNotEmpty()
+  @IsString()
+  lastName: string;
+
+  @ApiProperty({
     description: 'Email of the registered user',
     default: 'new-user@email.de'
   })
