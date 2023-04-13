9ed833ab1e3d656d39d477c0b7d149afb18a1a19
Sautino <marcelsauter9299@gmail.com>
2023-04-13 17:22:27 +0200

    Auto generation

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder } from '@nestjs/swagger';
 import { SwaggerModule } from '@nestjs/swagger/dist';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
